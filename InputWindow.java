564adb0ff0f0d550ad825d8db38f2ed480b2306f
Joseph Robson <joe_a_r@icloud.com>
2025-05-11 23:33:06 +0100

    Finalised InputWindow visuals

--- InputWindow.java
+++ InputWindow.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
@@ -11,6 +12,8 @@ import java.util.*;
 
 public class InputWindow extends JFrame implements ActionListener
 {
+    final Color backgroundColor = new Color(18, 22, 68);
+
     RoutePlanner routePlanner;
 
     JPanel mainPanel;
@@ -33,6 +36,7 @@ public class InputWindow extends JFrame implements ActionListener
         {
             nodeNames.add(n.getName());
         }
+        Collections.sort(nodeNames);
 
         setupWindow(nodeNames);
     }
@@ -44,13 +48,14 @@ public class InputWindow extends JFrame implements ActionListener
         //big title label
         JLabel titleLabel = new JLabel("Journey Planner");
         titleLabel.setFont(new Font("SansSerif", Font.BOLD, 40));
-        titleLabel.setForeground(Color.BLACK);
+        titleLabel.setForeground(Color.WHITE);
 
         //train icon
         JLabel iconLabel = new JLabel(trainIcon);
 
         //setup the title panel
-        titlePanel = new JPanel(new FlowLayout());
+        titlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        titlePanel.setBackground(backgroundColor);
         titlePanel.add(iconLabel);
         titlePanel.add(titleLabel);
 
@@ -58,8 +63,55 @@ public class InputWindow extends JFrame implements ActionListener
         startComboBox = new JComboBox<>(nodes.toArray(new String[0]));
         endComboBox = new JComboBox<>(nodes.toArray(new String[0]));
 
+        startComboBox.setSelectedIndex((int)(Math.random() * (nodes.size() - 1)));
+        endComboBox.setSelectedIndex((int)(Math.random() * (nodes.size() - 1)));
+
+        //setup planner panel
+        plannerPanel = new JPanel(new GridLayout(5, 1));
+        plannerPanel.setBackground(backgroundColor);
+        plannerPanel.add(new JLabel());
+
+        //slim size for combo boxes
+        Dimension comboSize = new Dimension(240, 60); // Adjust as needed
+
+        //start combobox setup
+        JPanel startPanel = new JPanel(new BorderLayout());
+        startPanel.setBackground(backgroundColor);
+        startPanel.add("West", new JLabel(new ImageIcon("images/start.png")));
+
+        //start combobox wrapper setup
+        JPanel startBoxWrapper = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        startBoxWrapper.setBackground(backgroundColor);
+        startComboBox.setPreferredSize(comboSize);
+        startBoxWrapper.add(startComboBox);
+        startPanel.add("Center", startBoxWrapper);
+
+        //travel panel icon setup
+        JPanel travelPanel = new JPanel(new BorderLayout());
+        travelPanel.setBackground(backgroundColor);
+        travelPanel.add("West", new JLabel(new ImageIcon("images/travel.png")));
+
+        //end combobox setup
+        JPanel endPanel = new JPanel(new BorderLayout());
+        endPanel.setBackground(backgroundColor);
+        endPanel.add("West", new JLabel(new ImageIcon("images/end.png")));
+
+        //end combobox wrapper setup
+        JPanel endBoxWrapper = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        endBoxWrapper.setBackground(backgroundColor);
+        endComboBox.setPreferredSize(comboSize);
+        endBoxWrapper.add(endComboBox);
+        endPanel.add("Center", endBoxWrapper);
+
+        plannerPanel.add(startPanel);
+        plannerPanel.add(travelPanel);
+        plannerPanel.add(endPanel);
+        plannerPanel.add(new JLabel());
+
+        /*
         //setup input panel
-        plannerPanel = new JPanel(new GridLayout(5, 5));
+        plannerPanel = new JPanel(new GridLayout(5, 4));
+        plannerPanel.setBackground(backgroundColor);
         fillPanel(plannerPanel, 6);
         plannerPanel.add(new JLabel(new ImageIcon("images/start.png")));
         plannerPanel.add(startComboBox);
@@ -71,6 +123,7 @@ public class InputWindow extends JFrame implements ActionListener
         plannerPanel.add(new JLabel(new ImageIcon("images/end.png")));
         plannerPanel.add(endComboBox);
         fillPanel(plannerPanel, 7);
+        */
 
         //go button
         goButton = new JButton("Go");
