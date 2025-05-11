cbc1c72165d60a1e695107b3167f9b3d37e25673
Joseph Robson <joe_a_r@icloud.com>
2025-05-11 23:48:06 +0100

    Created basics of OutputWindow

--- OutputWindow.java
+++ OutputWindow.java
@@ -4,7 +4,6 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Font;
-import java.awt.event.*;
 import java.awt.font.*;
 import javax.swing.*;
 import java.util.*;
@@ -13,14 +12,45 @@ public class OutputWindow extends JFrame
 {
     Route route;
 
+    JPanel mainPanel;
+    JScrollPane scrollPane;
+
     public OutputWindow(String source, String target, RoutePlanner routePlanner)
     {
         route = routePlanner.calculateRoute(source, target);
         route.output();
 
-        this.setSize(500, 500);
+        setupWindow(route);
+
+        this.setSize(500, 300);
         this.setVisible(true);
-        this.setTitle("Journey");
+        this.setContentPane(scrollPane);
+        this.setTitle(source + " -> " + target);
         this.setResizable(false);
+    }
+
+    public void setupWindow(Route route)
+    {
+        ArrayList<Edge> simpleRoute = route.getSimpleRoute();
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        addNode(simpleRoute.get(0).getStartNode(), mainPanel);
+        for(Edge e : simpleRoute)
+        {
+            addNode(e.getEndNode(), mainPanel);
+        }
+
+        scrollPane = new JScrollPane(mainPanel);
+    }
+
+    public void addNode(Node n, JPanel panel)
+    {
+        JLabel label = new JLabel(n.getName());
+        label.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        panel.add(label);
+        panel.add(Box.createVerticalStrut(5));
+
     }
 }
